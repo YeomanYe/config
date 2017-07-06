706c9a53934ce87d4a3cc05ebf4d03bb03e6e319
YeomanYe <ming13821007683@gmail.com>
2017-07-06 22:47:31 +0800

    update

--- auto_back.sh
+++ auto_back.sh
@@ -1,1 +1,3 @@
-cp ~/.spf13-vim-3/.vimrc ~/.spf13-vim-3/.vimrc.before ~/.spf13-vim-3/.vimrc.bundles ~/.vimrc.bundles.local ~/.vimrc.local /home/yeomanye/文档/Github_Repository/config/vim/ 
+#!/bin/bash -xv
+
+cp  ~/.vimrc.bundles.local ~/.vimrc.local /home/yeomanye/文档/Workspace/Github_Repository/config/vim/
