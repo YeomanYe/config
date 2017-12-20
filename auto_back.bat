98f94128f239c8047b9afa1207625990a0eed268
YeomanYe <ming13821007683@gmail.com>
2017-12-20 22:31:04 +0800

    add auto_back.bat'

---
+++ auto_back.bat
@@ -0,0 +1,3 @@
+xcopy "D:\Sublime Text 3\Data\Cache" "G:\Workspace\Github\config\Sublime Text 3\Cache" /s /e /h /y
+xcopy "D:\Sublime Text 3\Data\Installed Packages" "G:\Workspace\Github\config\Sublime Text 3\Installed Packages" /s /e /h /y
+xcopy "D:\Sublime Text 3\Data\Packages" "G:\Workspace\Github\config\Sublime Text 3\Packages" /s /e /h /y
