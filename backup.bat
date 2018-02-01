aa6f1f594fb3865269e05544011786b89f3b3eab
YeomanYe <ming13821007683@gmail.com>
2018-02-01 11:13:27 +0800

    add cmder

--- backup.bat
+++ backup.bat
@@ -1,5 +1,6 @@
 xcopy "D:\Sublime Text 3\Data\Cache" "G:\Workspace\Github\config\Sublime Text 3\Cache" /s /e /h /y
 xcopy "D:\Sublime Text 3\Data\Installed Packages" "G:\Workspace\Github\config\Sublime Text 3\Installed Packages" /s /e /h /y
 xcopy "D:\Sublime Text 3\Data\Packages" "G:\Workspace\Github\config\Sublime Text 3\Packages" /s /e /h /y
+xcopy "D:\cmder\vendor\conemu-maximus5\ConEmu.xml" "G:\Workspace\Github\config\cmder\ConEmu.xml" /c /y /h /r 
 xcopy "D:\TS_Studio v1.2.3\cfg\user.lua" "G:\Workspace\Github\config\touchsprite\studio\" /c /y /h /r 
 xcopy "D:\TSColorPicker_1.7.10\scripts\config\colorpicker\keymap.lua" "G:\Workspace\Github\config\touchsprite\colorpicker\" /c /y /h /r 
